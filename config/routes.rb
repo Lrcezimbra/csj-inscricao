0e454600f6051785989541546e57d478243dc81f
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2015-03-20 23:53:22 -0300

    Rotas configuradas (root e inscritos/ok)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :inscritos
+  resources :inscritos, :defaults => { :format => 'html' }
+  root 'inscritos#new'
+  get '/ok' => 'inscritos#ok'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
